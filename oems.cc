a03cc63a2b2aed4fc402e61f5d02c2c155db874a
Sebastian Krajnak <sebastian.krajnak@gmail.com>
2022-04-01 17:31:49 +0200

    i am losing hope

--- oems.cc
+++ oems.cc
@@ -6,7 +6,6 @@ int main(int argc, char *argv[]){
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    pritnf("I am %d of %d\n", rank, size);
+    printf("I am %d of %d\n", rank, size);
     MPI_Finalize();
     return EXIT_SUCCESS;
-}

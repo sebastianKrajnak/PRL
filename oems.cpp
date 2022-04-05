a6cf9d373fd68f1c9a2a9c1ef374d9c712a4b3b1
Sebastian Krajnak <sebastian.krajnak@gmail.com>
2022-04-05 19:56:43 +0200

    final

--- oems.cpp
+++ oems.cpp
@@ -1,3 +1,8 @@
+/*
+    Parallel Odd-even Merge Sort using OpenMPI
+    Author: Sebastian Krajnak
+    Date: 5.4.2022 
+*/
 #include <mpi.h>
 #include <iostream>
 #include <fstream>
@@ -5,8 +10,13 @@
 using namespace std;
 
 void printNumbers(int *numbers){
-    for(int i = 0; i < 8; i++){
-        cout << numbers[i] << ' ';
+    if(sizeof(numbers) == 0){
+        cout << "Error numbers not loaded!";
+    }
+    else{
+        for(int i = 0; i < 8; i++){
+            cout << numbers[i] << ' ';
+        }
     }
     cout << endl;
 }
@@ -28,6 +38,7 @@ int main(int argc, char *argv[]){
 
     int rank;
     int numbers[8] = {};
+    int receivedNums[2] = {};
     int sortedNums[8] = {};
     /*
         2D array of processors where: pos 0 and 1 are receiving processors of the 
@@ -40,20 +51,21 @@ int main(int argc, char *argv[]){
         {16,15}, {15,0}, {16,17}, {17,18},
         {0,0}, {0,0}, {0,0}
     };
+    /* 
+        Array of processors outputting correct sequence of sorted numbers, otherwise 
+        numbers would be sorted on the processors but they would be saved 
+        randomly, depending on who sent their answer first, in the array
+     */
     int outputProcs[8] = {10,16,16,17,17,18,18,13};
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    
+    // Master processor loads numbers from input binary file a sends them out to
+    // the first layer of sorting processors
     if(rank == 0){
-        // Load numbers from input binary file a send send them out to the first layer of sorting processors
         fstream srcFile;
         srcFile.open("numbers", ios::in);
-
-        if(!srcFile.is_open()){
-            cout << "Error: unable to open file numbers";
-            return {};
-        }
 
         while(srcFile.good()){
             int number = 0;
@@ -75,8 +87,7 @@ int main(int argc, char *argv[]){
         }
     }
 
-    int receivedNums[2] = {};
-
+    // Receive numbers from previous processors and send the min/max to the next set of processors
     MPI_Recv(&receivedNums[0], 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, nullptr);
     MPI_Recv(&receivedNums[1], 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, nullptr);
 
